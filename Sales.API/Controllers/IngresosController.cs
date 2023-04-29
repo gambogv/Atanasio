397cd72cdd96c65fa8b74e3670e129599aa6d330
GABRIEL ANGEL VELASQUEZ RINCON <gabrielvelasquez256153@correo.itm.edu.co>
2023-04-28 23:36:39 -0500

    Enlistar

---
+++ Sales.API/Controllers/IngresosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entites;
+
+namespace Sales.API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/ingresos")]
+    
+    public class IngresosController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public IngresosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Ingresos.ToListAsync());
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Ingreso ingreso)
+        {
+            _context.Add(ingreso);
+            await _context.SaveChangesAsync();
+            return Ok(ingreso);
+        }
+
+
+    }
+}
